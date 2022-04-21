5a41f8ddb5b0f1ec1aaeefa8c275ec55382f5ef8
Rafael Yumagulov <yumagulovrn@gmail.com>
2022-04-22 02:36:41 +0300

    feat(leetcode): added solutions for [0011] Container With Most Water

--- src/leetcode/mod.rs
+++ src/leetcode/mod.rs
@@ -8,3 +8,4 @@ mod p0007_reverse_integer;
 mod p0008_string_to_integer_atoi;
 mod p0009_palindrome_number;
 mod p0010_regular_expression_matching;
+mod p0011_container_with_most_water;
---
+++ src/leetcode/p0011_container_with_most_water.rs
@@ -0,0 +1,72 @@
+/**
+ * [0011] Container With Most Water
+ *
+ * You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+ * Find two lines that together with the x-axis form a container, such that the container contains the most water.
+ * Return the maximum amount of water a container can store.
+ * Notice that you may not slant the container.
+ *
+ * Example 1:
+ * Input: height = [1,8,6,2,5,4,8,3,7]
+ * Output: 49
+ * Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+ *
+ * Example 2:
+ * Input: height = [1,1]
+ * Output: 1
+ *
+ * Constraints:
+ * * n == height.length
+ * * 2 <= n <= 10^5
+ * * 0 <= height[i] <= 10^4
+ */
+struct Solution;
+
+impl Solution {
+    pub fn max_area(height: Vec<i32>) -> i32 {
+        let (mut max_area, mut l, mut r) = (0, 0, height.len() - 1);
+        while l < r {
+            max_area = max_area.max(height[l].min(height[r]) * (r - l) as i32);
+            if height[l] < height[r] {
+                l += 1;
+            } else {
+                r -= 1;
+            }
+        }
+        max_area
+    }
+
+    pub fn max_area_iter(height: Vec<i32>) -> i32 {
+        let mut max_area = 0;
+        let mut i = height.iter().enumerate();
+        let mut il = i.next();
+        let mut ir = i.next_back();
+
+        while let (Some((l, lh)), Some((r, rh))) = (il, ir) {
+            max_area = max_area.max(lh.min(rh) * (r - l) as i32);
+            if lh < rh {
+                il = i.next();
+            } else {
+                ir = i.next_back();
+            }
+        }
+        max_area
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_max_area() {
+        assert_eq!(Solution::max_area(vec![1, 8, 6, 2, 5, 4, 8, 3, 7]), 49);
+        assert_eq!(Solution::max_area(vec![1, 1]), 1);
+    }
+
+    #[test]
+    fn test_max_area_iter() {
+        assert_eq!(Solution::max_area_iter(vec![1, 8, 6, 2, 5, 4, 8, 3, 7]), 49);
+        assert_eq!(Solution::max_area_iter(vec![1, 1]), 1);
+    }
+}
